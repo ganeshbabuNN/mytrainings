3a75e39c1338be37f70de3e79a86b14eb38b94b7
ganeshbabug <ganeshbabu346@gmail.com>
2026-01-21 22:26:12 +0530

    .

---
+++ UDS_2.0_KrishNaik/Practice_Level/level_1/lectures/test.py
@@ -0,0 +1,5 @@
+name_1= input("Enter your name")
+name_2= input("Enter your name ")
+name_3= input("Enter your name :")
+name_4= input("Enter your name : ")
+print(name_1,name_2,name_3,name_4)
--- UDS_2.0_KrishNaik/Running_Notes.py
+++ UDS_2.0_KrishNaik/Running_Notes.py
@@ -30,7 +30,7 @@ uv python pin 3.12
 #c:/Users/ganes/AppData/Roaming/uv/python/cpython-3.12.12-windows-x86_64-none/python.exe -m pip install ipykernel -U --user --force-reinstall
 
 #Create environment 
-uv venv level_1_venv #using environment with name suggest to suffix as _venv
+uv venv level1__venv #using environment with name suggest to suffix as _venv
 level_1_venv\Scripts\activate  #activate the /env
 level_1_venv\Scripts\deactivate  #deactivate if you want
 python -m ensurepip --upgrade #The ensurepip module is a "fallback" built into Python itself. It doesn't need to be downloaded; it's there specifically to "ensure" that pip is installed in your current environment when it's missing
@@ -49,7 +49,17 @@ python -m venv .venv ##python way if the path -TBE
 
 
 #operators are special symbols
-#monal is using cyberpunk theme
+#monal is using cyberpunk theme in this VSCODE
+
+
+#Assignment1
+#data_science--> day_1-->day_1.tx
+#            --> day_2-->lectures--> day_2.ipynb
+#            --> Notes -->notes.txt
+
+#assignment -2
+## revise all the code and concept
+## do type casting from all combinations & understand which are comptiable
 
 
 ##2026018 - class
@@ -60,3 +70,16 @@ python -m venv .venv ##python way if the path -TBE
 #control flow
 #control flow -II(advanced)
 #non primitive data type.
+
+
+##2026021 - class
+#=================
+#agenda
+#input
+#output
+#control flow(advance)
+#loop
+
+
+#turning of the code  suggestion
+#file -> perferences --> settings --> editor.inlineSuggest.enabled=false
