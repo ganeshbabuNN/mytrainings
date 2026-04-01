779bab89202bf9a8a3c06b1d820bc35fbf8352cb
ganeshbabug <ganeshbabu346@gmail.com>
2026-04-01 13:23:26 +0530

    .

---
+++ R_Training_Swetha_20260105/R_Walk_Through_Notes_Day58.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(nycflights13)  
+
+
+flights
+view(flights)
+flights <- flights #not valid
+#load the data
+data("flights")
+data("airlines")
+data("airports")
+data("weather")
+data("planes")
